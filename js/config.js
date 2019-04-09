3b1cb6faef1e1323acc656f321a9a00452071869
sluxqt <sluxqt@gmail.com>
2019-04-09 15:07:35 +0200

    MindQ.dk v1.2
    Updated the MindQ GitHub to version 1.2. (Final)

--- js/config.js
+++ js/config.js
@@ -4,13 +4,13 @@ $(function() {
 	    // array representing the format and columns of the cart, see
 	    // the cart columns documentation
 	    cartColumns: [
-	        { attr: "name" , label: "Name" },
-	        { attr: "price" , label: "Price", view: 'currency' },
+	        { attr: "name" , label: "Produkt" },
+	        { attr: "price" , label: "Pris", view: 'currency' },
 	        { view: "decrement" , label: false },
-	        { attr: "quantity" , label: "Qty" },
+	        { attr: "quantity" , label: "Antal" },
 	        { view: "increment" , label: false },
 	        { attr: "total" , label: "SubTotal", view: 'currency' },
-	        { view: "remove" , text: "Remove" , label: false }
+	        { view: "remove" , text: "Fjern" , label: false }
 	    ],
 
 	    // "div" or "table" - builds the cart as a table or collection of divs
@@ -19,18 +19,18 @@ $(function() {
 	    // how simpleCart should checkout, see the checkout reference for more info
 	    checkout: {
 	        type: "PayPal" ,
-	        email: "you@yours.com"
+	        email: "m.baryske@gmail.com"
 	    },
 
 	    // set the currency, see the currency reference for more info
-	    currency: "USD",
+	    currency: "DKK",
 
 	    // collection of arbitrary data you may want to store with the cart,
 	    // such as customer info
 	    data: {},
 
 	    // set the cart langauge (may be used for checkout)
-	    language: "english-us",
+	    language: "danish-da",
 
 	    // array of item fields that will not be sent to checkout
 	    excludeFromCheckout: [
@@ -73,10 +73,10 @@ $(function() {
 	simpleStore.init({
 
 		// brand can be text or image URL
-		brand : "SimpleStore",
+		brand : "MindQ Clothing",
 
 		// numder of products per row (accepts 1, 2 or 3)
-		numColumns : 3,
+		numColumns : 2,
 
 		// name of JSON file, located in directory root
 		JSONFile : "products.json"
---
+++ js/main.js
@@ -0,0 +1,94 @@
+/* -------------------------------------------------------
+                     Sticky Menu
+-------------------------------------------------------- */
+
+// Create a clone of the menu, right next to original.
+$('.header').addClass('original').clone().insertAfter('.header').addClass('cloned').css('position','fixed').css('top','0').css('margin-top','0').css('z-index','500').removeClass('original').hide();
+
+scrollIntervalID = setInterval(stickIt, 10);
+
+function stickIt() {
+
+  var orgElementPos = $('.original').offset();
+  orgElementTop = orgElementPos.top;               
+
+  if ($(window).scrollTop() >= (orgElementTop)) {
+    // scrolled past the original position; now only show the cloned, sticky element.
+
+    // Cloned element should always have same left position and width as original element.     
+    orgElement = $('.original');
+    coordsOrgElement = orgElement.offset();
+    leftOrgElement = coordsOrgElement.left;  
+    widthOrgElement = orgElement.css('width');
+    $('.cloned').css('left',leftOrgElement+'px').css('top',0).css('width',widthOrgElement).show();
+    $('.original').css('visibility','hidden');
+  } else {
+    // not scrolled past the menu; only show the original menu.
+    $('.cloned').hide();
+    $('.original').css('visibility','visible');
+  }
+}
+
+/* -------------------------------------------------------
+                     Navigation Underlines
+-------------------------------------------------------- */
+
+var sections = $('section')
+  , nav = $('nav')
+  , header = $('header')
+  , nav_height = header.outerHeight();
+
+$(window).on('scroll', function () {
+  var cur_pos = $(this).scrollTop();
+  
+  sections.each(function() {
+    var top = $(this).offset().top - nav_height - 60,
+        bottom = top + $(this).outerHeight();
+    
+    if (cur_pos >= top && cur_pos <= bottom) {
+      nav.find('a').removeClass('active');
+      sections.removeClass('active');
+      
+      $(this).addClass('active');
+      nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
+    }
+  });
+});
+
+/* -------------------------------------------------------
+                     Navgation Scroll animation
+-------------------------------------------------------- */
+
+sections.find('a.cd-btn').on('click', function () {
+  var $el = $(this)
+    , id = $el.attr('href');
+  
+  $('html, body').animate({
+    scrollTop: $(id).offset().top - nav_height
+  }, 500);
+  
+  return false;
+});
+
+nav.find('a').on('click', function () {
+  var $el = $(this)
+    , id = $el.attr('href');
+  
+  $('html, body').animate({
+    scrollTop: $(id).offset().top - nav_height
+  }, 500);
+  
+  return false;
+});
+
+
+header.find('a.simpleStore_viewCart').on('click', function () {
+  var $el = $(this)
+    , id = $el.attr('href');
+  
+  $('html, body').animate({
+    scrollTop: $(id).offset().top - nav_height
+  }, 500);
+  
+  return false;
+});
